0dbfd6db1b19783395487a01a3b06039beb478c0
Tyler Neylon <tylerneylon@gmail.com>
2021-08-24 09:58:50 -0700

    Add the h command to show your solution history.

--- cryptogram.py
+++ cryptogram.py
@@ -18,18 +18,33 @@
     > ?   --> Show help (this message).
     > s   --> Shuffle all letters randomly.
     > c   --> Show common short English words.
+    > h   --> Print an abbreviated work history, for sharing your process.
     > q   --> Quit.
 """
+# TODO:
+#   Currently, when printing out your history (the `h` command),
+#   shuffles will appear as a long list of swaps. It would be nicer
+#   to have a single line for a shuffle that presents it as such.
+#
 
 
 # ____________________________________________________________
 # Imports
 
 import random
+import subprocess
 import sys
 
 from collections import Counter
 
+
+# ____________________________________________________________
+# Globals and constants
+
+# These will store terminal escape codes.
+# Currently these are used by print_with_highlights().
+highlight_seq = None
+reset_seq     = None
 
 # ____________________________________________________________
 # Functions
@@ -46,6 +61,39 @@ def show_letter_frequencies(s):
     letters = [letter for letter, _ in letter_counts.most_common()[:10]]
     print('Cipher letters: ', ' '.join(letters))
     print('English letters:', 'e t a o i n s r h d')
+
+def get_cmd_output(cmd):
+    """ Return stdout, as a bytes object, from running `cmd`. """
+    process_completion = subprocess.run(cmd.split(), capture_output=True)
+    return process_completion.stdout
+
+def print_with_highlights(s, highlights):
+    """ Print out the string s while highlighting characters in the string
+        `highlights`.
+    """
+    global highlight_seq, reset_seq
+
+    if highlight_seq is None:
+        highlight_seq = get_cmd_output('tput setaf 7')
+        reset_seq     = get_cmd_output('tput sgr0')
+
+    bytes_to_print = []
+    for c in s:
+        c_byte = c.encode()
+        if c in highlights:
+            bytes_to_print += [highlight_seq, c_byte, reset_seq]
+        else:
+            bytes_to_print.append(c_byte)
+    sys.stdout.buffer.write(b''.join(bytes_to_print + [b'\n']))
+
+def show_history(crypt, swaps):
+    """ Show the swap history of the cryptogram. """
+    s = crypt
+    print(f'Start   {s}')
+    for pair in swaps:
+        s = swap(s, pair[0], pair[1])
+        print(f'{pair[0]}<->{pair[1]}   ', end='', flush=True)
+        print_with_highlights(s, pair)
 
 def show_in_columns(words, col_width=6, max_width=65, indent=4):
     """ Print the words in `words` in colums of width `col_width`, in lines of
@@ -142,6 +190,9 @@ while True:
 
     elif inp == 'c':
         show_common_elements()
+
+    elif inp == 'h':
+        show_history(crypt, swaps)
 
     elif inp == 'q':
         print('Have a great day! :D')
